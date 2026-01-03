3d497f0b4bcf76f57dbdb0345dab292d7e477146
+ <+>
2026-01-03 18:55:53 +0300

    solution

--- server/app.py
+++ server/app.py
@@ -7,19 +7,42 @@ from flask_migrate import Migrate
 from models import db, Pet
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    body = {'message': 'Welcome to the pet directory!'}
+    body = {"message": "Welcome to the pet directory!"}
     return make_response(body, 200)
 
 
-if __name__ == '__main__':
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        body = {"message": f"Pet {id} not found."}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route("/species/<string:species>")
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+    body = {"count": len(pets), "pets": pets}
+    return make_response(body, 200)
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
